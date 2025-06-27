8998d5202528fe6719134a7a08af69838aefc6c2
Xavier Teague <xaviervt1994@gmail.com>
2025-06-27 16:26:27 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,4 @@
+// Import global CSS so Vite includes it in the build
+import "./style.css";
+
+console.log("Teague Web Studio loaded.");
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/teaguewebstudio/", // Make sure this matches your GitHub repo name exactly
+});
